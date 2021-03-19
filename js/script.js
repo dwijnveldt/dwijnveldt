7d3fc486d58959dcc772f6336df8f5eef5ed716b
dwijnveldt <80924043+dwijnveldt@users.noreply.github.com>
2021-03-19 22:45:09 +0100

    new stuff

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
-// Get the modal
+// create references to the modal...
 var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('masonry-content');
+// the image in the modal
 var modalImg = document.getElementById("img01");
+// and the caption in the modal
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+    var img = images[i];
+    // and attach our click listener for this image.
+    img.onclick = function(evt) {
+        console.log(evt);
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
 }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("modal")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+span.onclick = function() {
     modal.style.display = "none";
 }
